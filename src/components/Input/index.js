7aa854b5d505916e8300a943f1296ffbcb5a287f
Erick G. Peroza <perozaerick@protonmail.com>
2023-02-26 23:44:49 -0300

    added stylization

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./index.css";
 
-const Input = ({ id, classes, value, onChangeFunction }) => {
+const Input = ({ id, classes, type, value, onChangeFunction, isDisabled }) => {
   return (
-    <input id={id} className={classes} onChange={onChangeFunction}>
+    <input id={id} className={classes} type={type} onChange={onChangeFunction} disabled={isDisabled}>
       {value}
     </input>
   );
