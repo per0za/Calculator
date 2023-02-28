9ec911c0ebd98fd6461b4921088edd2ecf9da19f
Erick G. Peroza <perozaerick@protonmail.com>
2023-02-28 16:20:02 -0300

    themeswitch function

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,10 +3,29 @@ import Button from "../Button";
 import "./index.css";
 
 const Header = () => {
+  const themeSwitch = () => {
+    const main = document.querySelector("main");
+    const root = document.querySelector(":root");
+
+    if (main.dataset.theme === "dark") {
+      root.style.setProperty("--bg-color", "#f1f5f9");
+      root.style.setProperty("--border-color", "#aaa");
+      root.style.setProperty("--font-color", "#212529");
+      root.style.setProperty("--primary-color", "#26834a");
+      main.dataset.theme = "light";
+    } else {
+      root.style.setProperty("--bg-color", "#212529");
+      root.style.setProperty("--border-color", "#666");
+      root.style.setProperty("--font-color", "#f1f5f9");
+      root.style.setProperty("--primary-color", "#4dff91");
+      main.dataset.theme = "dark";
+    }
+  };
+
   return (
     <header className="flex align-center justify-between">
       <h1>Calculator</h1>
-      <Button id={"themeSwitcher"} value={"Switch Theme Dark/Light"}></Button>
+      <Button id={"themeSwitcher"} value={"Switch Theme Dark/Light"} onClickFunction={themeSwitch}></Button>
     </header>
   );
 };
