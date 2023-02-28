f89be18668c305973a97514c24a62315a2034340
Erick G. Peroza <perozaerick@protonmail.com>
2023-02-28 15:34:31 -0300

    added new component section

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,6 +1,15 @@
 import React from "react";
+import Button from "../Button";
+import Input from "../Input";
 import "./index.css";
 
 const Section = () => {
-  return;
+  return (
+    <section>
+      <Button id={"copyToClipboard"} value={"Copy"}></Button>
+      <Input type={"text"} id={"result"} isDisabled={true}></Input>
+    </section>
+  );
 };
+
+export default Section;
